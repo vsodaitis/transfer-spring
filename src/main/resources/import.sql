8b5622ec40fb70dc98e386ec34fa0175c27f025f
Vytenis Sodaitis <v.sodaitis@ba.lt>
2019-05-08 13:41:40 +0300

    use postgresql; add docker-compose

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
-INSERT INTO user_entity (id, full_name) VALUES (NEXT VALUE FOR hibernate_sequence, 'Viktor Tsoy');
+INSERT INTO user_entity (id, full_name) VALUES (nextval('hibernate_sequence'), 'Viktor Tsoy');
 
-INSERT INTO balance_entity (id, total, reserved) VALUES (NEXT VALUE FOR hibernate_sequence, 100, 90);
-INSERT INTO balance_entity (id, total, reserved) VALUES (NEXT VALUE FOR hibernate_sequence, 600, 11);
+INSERT INTO balance_entity (id, total, reserved) VALUES (nextval('hibernate_sequence'), 100, 90);
+INSERT INTO balance_entity (id, total, reserved) VALUES (nextval('hibernate_sequence'), 600, 11);
 
-INSERT INTO account_entity (id, user_id, status, balance_id, iban, currency) VALUES (NEXT VALUE FOR hibernate_sequence, 1, 'ACTIVE', 2, 'LT4645464564', 'EUR');
-INSERT INTO account_entity (id, user_id, status, balance_id, iban, currency) VALUES (NEXT VALUE FOR hibernate_sequence, 1, 'ACTIVE', 3, 'RB3151564887', 'USD');
+INSERT INTO account_entity (id, user_id, status, balance_id, iban, currency) VALUES (nextval('hibernate_sequence'), 1, 'ACTIVE', 2, 'LT4645464564', 'EUR');
+INSERT INTO account_entity (id, user_id, status, balance_id, iban, currency) VALUES (nextval('hibernate_sequence'), 1, 'ACTIVE', 3, 'RB3151564887', 'USD');
 
-INSERT INTO user_entity (id, full_name) Values (NEXT VALUE FOR hibernate_sequence, 'John Smith');
+INSERT INTO user_entity (id, full_name) Values (nextval('hibernate_sequence'), 'John Smith');
 
-INSERT INTO balance_entity (id, total, reserved) VALUES (NEXT VALUE FOR hibernate_sequence, 500, 0);
+INSERT INTO balance_entity (id, total, reserved) VALUES (nextval('hibernate_sequence'), 500, 0);
 
-INSERT INTO account_entity (id, user_id, status, balance_id, iban, currency) VALUES (NEXT VALUE FOR hibernate_sequence, 6, 'ACTIVE', 7, 'LT3212131544', 'EUR');
+INSERT INTO account_entity (id, user_id, status, balance_id, iban, currency) VALUES (nextval('hibernate_sequence'), 6, 'ACTIVE', 7, 'LT3212131544', 'EUR');
